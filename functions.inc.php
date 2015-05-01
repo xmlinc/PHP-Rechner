74423cb61919883c2259f53ee3140462b4e0aede
xmlinc <support@softinc.de>
2015-05-02 01:06:09 +0200

    1.2

--- functions.inc.php
+++ functions.inc.php
@@ -1,155 +1,117 @@
 <?php
 
-	$home = './';
-//	$home = $_SERVER['PHP_SELF'];
-	$ausgabe = "";
-	$memory = "";	
-	$ergebnis = "";
-	$zahl1 = "";
-	$zahl2 = "";
-	$sumop = "";
-	$operator = "";
-	$op = "";
+$zahl1 = "";
+$zahl2 = "";
+$op = "";
+$sumop= "";
+$ausgabe = "";
+$ergebnis = "";
 
 //############    Ergebnis ausrechnen   ############//
 
-	function berechne($zahl1,$zahl2,$op)
+function berechne($zahl1,$zahl2,$op)
+{
+	$ergebnis = "";
+
+	switch ($op)
 	{
-		$ergebnis="";
-		
-		switch ($op)
-		{
 		case "+" : $ergebnis = $zahl1 + $zahl2;
-				break;
+			break;
 		case "-" : $ergebnis = $zahl1 - $zahl2;
-				break;		
-		case "*" : $ergebnis = $zahl1 * $zahl2;
-				break;
-		case "/" : $ergebnis = $zahl1 / $zahl2;
-				break;
+			break;
+		case "×" : $ergebnis = $zahl1 * $zahl2;
+			break;
+		case "÷" : $ergebnis = $zahl1 / $zahl2;
+			break;
 	}
 	return $ergebnis;
 }
 
-//############    Ergebnis ausrechnen   ############//
- 	
-	if(isset($_POST['sum']))
+//############    Erste Zahl merken   ############//
+
+	if(isset($_POST['zahl']))
 	{
-		$zahl1 = $_POST['zahl1'];
-		$zahl2 = $_POST['output'];
-		$op    = $_POST['oper'];
-		$ausgabe = berechne($zahl1,$zahl2,$op);
-		$sumop = "=";
+		if ($_POST['output'] == "0" )
+//		if (($_POST['output'] == "0" )&&($_POST['zahl'] == "0"))
+		{
+//			$ausgabe = "0";
+			$zahl1 = $_POST['zahl1'];
+			$op = $_POST['ope'];
+		}
+		else if($zahl1 == "")
+		{
+			$ausgabe = "";
+			$ausgabe .= $_POST['output'];
+			$ausgabe .= $_POST['zahl'];
+			$zahl1 = $_POST['zahl1'];
+			$op = $_POST['ope'];
+		}
+		else
+		{
+			$ausgabe .= $_POST['output'];
+			$ausgabe .= $_POST['zahl'];
+		}
 	}
-
-//############   Display Ausgabe   ############//
-
-	function ausgabe()
-	{	
-		global $zahl1,$zahl2,$op,$ausgabe;
-		
-		if(isset($_POST['op']))
-		{
-			$zahl1 = $_POST['output'];
- 		}
- 		else if(isset($_POST['zahl']))
-		{
-			if (($_POST['output'] == "" )&&($_POST['zahl'] == "0"))
-			{
-				$ausgabe = "";
-			}
-			else
-			{
-//				$ausgabe = "";
-				$ausgabe .= $_POST['output'];
-				$ausgabe .= $_POST['zahl'];
-			}
-		}
-		else if(isset($_POST['sum']))
-		{
-			$ausgabe = berechne($zahl1,$zahl2,$op);
-		}
-		else if(isset($_POST['neg']))
-		{
-			$ausgabe = $_POST['output'];
-			if($ausgabe > 0) // auf das vorhanden eines minus zeichensin der kette prüfen, wenn ja, entfernen			{
-			{
-			$ausgabe = "-";
-				$ausgabe .= $_POST['output'];
-			}
-			else if($ausgabe < 0)
-			{
-//				$ausgabe = $_POST['output'];
-//				$ausgabe = strlen($ausgabe)-1;
-//				$ausgabe = substr($string,1,$laenge);		
-				$ausgabe = str_replace('-', '', $ausgabe);
-			}
-		}
-		
-	echo $ausgabe;
-	}
-
-//############    Operator ausführen   ############//
-
-	if((isset($_POST['sumop']))&&(($_POST['sumop']) != ""))
-	{
-		$zahl1 = $_POST['output'];
-		$zahl2 = "";
-//	$sumop = "";
-		$operator = "";
-//		$sumop = '=';
-//		$operator = $_POST['op'];
- 	}
 
 //############    Operator ausführen   ############//
 
 	if(isset($_POST['op']))
 	{
-		$zahl1 = $_POST['output'];
-		$operator = $_POST['op'];
- 	}
+			$zahl1 = $_POST['output'];
+			$op = $_POST['op'];
+	}
 
-//############    Operator merken   ############//
- 			
-	if((isset($_POST['oper']))&&(($_POST['oper'])!="")&&(($_POST['zahl1']) != ""))
-	{
-		$operator = $_POST['oper'];
-		$memory = "";
-//		$sumop = "";
- 	}
+//############    Summe ausrechnen   ############//
 
-//############   Erste Zahl merken   ############//
- 		
- 	if((isset($_POST['zahl1']))&&(($_POST['zahl1'])!=""))
+	if(isset($_POST['sum']))
 	{
+		$zahl1 =$_POST['zahl1'];
+		$op = $_POST['ope'];
+		$zahl2 = $_POST['output'];
+		$ausgabe = berechne($zahl1,$zahl2,$op);
+		$ergebnis = $ausgabe;
+		$sumop = "=";
+	}
+
+//############    Negative / Positive Zahl   ############//
+
+	if (isset($_POST['neg']))
+	{
+		if($_POST['output']>0)
+		{
+			$ausgabe = "-";
+			$ausgabe .= $_POST['output'];
+		}
+		else
+		{
+			$ausgabe = $_POST['output'];
+			$ausgabe = str_replace('-','',$ausgabe);
+		}
+
 		$zahl1 = $_POST['zahl1'];
- 	}
+		$op = $_POST['ope'];
+	}
 
-//############   Zweite Zahl merken   ############//
- 		
- 	if((isset($_POST['zahl2']))&&(($_POST['zahl2'])!=""))
-	{
-		$zahl2 = $_POST['zahl2'];
- 	}
- 		
-//############   Rechner Reset ( Taste C )   ############//
+//############    Komma-Eingabe behandeln   ############//
 
- 	if(isset($_POST['cancel']))
+	if (isset($_POST['punkt']))
 	{
-		$operator = "";
-		$ergebnis = "";
-		$zahl1 = "";
-		$zahl2 = "";
-		$sumop = "";
-		$memory = "";
- 	}
- 	
- //############    Memory   ############//
-	
-// 	if((isset($_POST['mem']))&&(!isset($_POST['cancel']))&&(!isset($_POST['op']))&&(!isset($_POST['sum']))&&(!isset($_POST['cancel'])))
-//	{
-//		$memory .= $_POST['mem'];
-//		$memory .= $_POST['zahl'];
-//	}
-	
-?>	
+		$ausgabe = $_POST['output'];
+		$findMich   = '.';
+		$ausgabe = strpos($ausgabe, $findMich);
+
+		if ($ausgabe == false)
+		{
+			$ausgabe = $_POST['output'];
+			$ausgabe .= ".";
+		}
+		else {
+			$ausgabe = $_POST['output'];
+		}
+
+		$zahl1 =$_POST['zahl1'];
+		$op = $_POST['ope'];
+	}
+
+//------   Ende PHP Programm   -----//
+?>
--- index.php
+++ index.php
@@ -1,1 +1,51 @@
-<?php require('functions.inc.php'); ?><!DOCTYPE html><html><head>	<meta charset="utf-8">	<title>php rechner</title>	<link href="style.css" type="text/css" rel="stylesheet" /></head><body><div id="wrapper">	<form action="<?php echo $home;?>" method="post">		<div id="fieldset">			<legend>php rechner</legend>			<input type="text"   name="output" value="<?php ausgabe(); ?>" id="output" readonly="readonly">			<input type="submit" name="cancel" value="c"   id="cancel">			<input type="submit" name="zahl"   value="1"   id="button">			<input type="submit" name="zahl"   value="2"   id="button">			<input type="submit" name="zahl"   value="3"   id="button">			<input type="submit" name="op"     value="+"   id="button">			<input type="submit" name="zahl"   value="4"   id="button">			<input type="submit" name="zahl"   value="5"   id="button">			<input type="submit" name="zahl"   value="6"   id="button">			<input type="submit" name="op"     value="-"   id="button">			<input type="submit" name="zahl"   value="7"   id="button">			<input type="submit" name="zahl"   value="8"   id="button">			<input type="submit" name="zahl"   value="9"   id="button">			<input type="submit" name="op"     value="*"   id="button">			<input type="submit" name="zahl"   value="."   id="button">			<input type="submit" name="zahl"   value="0"   id="button">			<input type="submit" name="neg"    value="+/-" id="button">			<input type="submit" name="op"     value="/"   id="button">					<input type="submit" name="sum"    value="="   id="sum">		</div>		<div id="history">			<input type="text" name="zahl1"  id="out"  value="<?php echo $zahl1; ?>">					<input type="text" name="oper"   id="out"  value="<?php echo $operator; ?>">					<input type="text" name="zahl2"  id="out"  value="<?php echo $zahl2; ?>">					<input type="text" name="sumop"  id="out"  value="<?php echo $sumop; ?>">			<input type="text" name="erg"    id="out"  value="<?php echo $ausgabe; ?>">			<input type="text" name="mem"    id="out"  value="<?php // echo $memory; ?>">		</div>	</form></div></body></html>
+<?php require('functions.inc.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>php rechner</title>
+	<link href="style.css" type="text/css" rel="stylesheet" />
+	</head>
+<body>
+<div id="wrapper">
+	<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+		<div id="fieldset">
+			<legend>php rechner</legend>
+			<input type="text"   name="output" value="<?php echo $ausgabe; ?>" id="output" readonly="readonly">
+			<input type="submit" name="cancel" value="C"   id="cancel">
+			<input type="submit" name="zahl"   value="1"   id="button">
+			<input type="submit" name="zahl"   value="2"   id="button">
+			<input type="submit" name="zahl"   value="3"   id="button">
+			<input type="submit" name="op"     value="+"   id="button">
+			<input type="submit" name="zahl"   value="4"   id="button">
+			<input type="submit" name="zahl"   value="5"   id="button">
+			<input type="submit" name="zahl"   value="6"   id="button">
+			<input type="submit" name="op"     value="-"   id="button">
+			<input type="submit" name="zahl"   value="7"   id="button">
+			<input type="submit" name="zahl"   value="8"   id="button">
+			<input type="submit" name="zahl"   value="9"   id="button">
+			<input type="submit" name="op"     value="×"   id="button">
+			<input type="submit" name="neg"    value="+/-" id="button">
+			<input type="submit" name="zahl"   value="0"   id="button">
+			<input type="submit" name="punkt"  value="."   id="button">
+			<input type="submit" name="op"     value="÷"   id="button">
+			<input type="submit" name="sum"    value="="   id="sum">
+		</div>
+		<div id="history">
+			<label>Duales Zahlensystem:</label>
+			<input type="text" name="dual"   id="out"   value="<?php echo decbin($ausgabe); ?>">
+			<label>Dezimales Zahlensystem:</label>
+			<input type="text" name="dual"   id="out"   value="<?php echo $ausgabe; ?>">
+			<label>Hexadezimales Zahlensystem:</label>
+			<input type="text" name="dual"   id="out"   value="<?php echo dechex($ausgabe); ?>">
+			<hr />
+			<input type="text" name="zahl1"  id="out"   value="<?php echo $zahl1; ?>">
+			<input type="text" name="ope"    id="out"   value="<?php echo $op; ?>">
+			<input type="text" name="zahl2"  id="out"   value="<?php echo $zahl2; ?>">
+			<input type="text" name="sumop"  id="out"   value="<?php echo $sumop; ?>">
+			<input type="text" name="erg"    id="out"   value="<?php echo $ergebnis; ?>">
+		</div>
+	</form>
+</div>
+</body>
+</html>
