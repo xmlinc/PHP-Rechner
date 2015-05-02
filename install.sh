431c2ea5793846a6aad74e2ffd0472821222e64a
xmlinc <support@softinc.de>
2015-05-02 02:20:08 +0200

    mv test

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
 git clone https://github.com/xmlinc/PHP-Rechner.git
-cd PHP-Rechner
-rm -rfv .git .gitignore install.sh
+mv -v PHP-Rechner/* ./
+#cd PHP-Rechner
+rm -rfv .git .gitignore install.sh PHP-Rechner
+#mv ./* ../ && cd ../
+#rm -rfv ./PHP-Rechner
